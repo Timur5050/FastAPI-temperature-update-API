6db26a8c3877b100138f60aebe8276896549ede2
timur <stukantimur811@gmail.com>
2024-09-03 21:02:49 +0300

    added async db connect

--- settings.py
+++ settings.py
@@ -5,6 +5,7 @@ class Settings(BaseSettings):
     PROJECT_NAME: str = "Temperature Management API"
 
     DATABASE_URL: str | None = "sqlite:///./cities_temperatures.sqlite3"
+    ASYNC_DATABASE_URL: str | None = "sqlite+aiosqlite:///./cities_temperatures.sqlite3"
 
     class Config:
         case_sensitive = True
