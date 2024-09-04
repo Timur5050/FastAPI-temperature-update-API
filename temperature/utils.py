ec0afe241206cd7ed603e1665c31d37f13190126
timur <stukantimur811@gmail.com>
2024-09-04 10:15:43 +0300

    changed async func of getting temperature

--- temperature/utils.py
+++ temperature/utils.py
@@ -1,33 +1,13 @@
-import requests
+import os
+import httpx
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.getenv("API_KEY")
 
 
-def get_location_key(city_name, api_key):
-    base_url = "http://dataservice.accuweather.com/locations/v1/cities/search"
-    params = {
-        'apikey': api_key,
-        'q': city_name,
-    }
-
-    response = requests.get(base_url, params=params)
-    data = response.json()
-
-    if response.status_code == 200 and len(data) > 0:
-        return data[0]['Key']
-
-
-def get_city_temperature(location_key, api_key):
-    base_url = f"http://dataservice.accuweather.com/currentconditions/v1/{location_key}"
-    params = {
-        'apikey': api_key,
-        'details': 'true'
-    }
-
-    response = requests.get(base_url, params=params)
-    data = response.json()
-
-    if response.status_code == 200 and len(data) > 0:
-        temperature = data[0]['Temperature']['Metric']['Value']
-        return temperature
-
-
-api_key = "lUjc5nMDM7fqrMcDcK4ohOIG1JEgUULD"
+async def get_city_temperature(city_name: str):
+    async with httpx.AsyncClient() as client:
+        res = await client.get(f"http://api.weatherapi.com/v1/current.json?key={api_key}&q={city_name}&aqi=no")
+        return res.json()["current"]["temp_c"]
