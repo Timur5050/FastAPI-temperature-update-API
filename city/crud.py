6d9515699b63b1e9ee5f3009e064fa85b762da8f
timur <stukantimur811@gmail.com>
2024-09-04 10:18:08 +0300

    added annotation

--- city/crud.py
+++ city/crud.py
@@ -1,14 +1,15 @@
 from sqlalchemy.orm import Session
+from typing_extensions import List
 
 from city.models import DBCity
 from city.schemas import City
 
 
-def get_all_cities(db: Session):
+def get_all_cities(db: Session) -> List[City]:
     return db.query(DBCity).all()
 
 
-def create_city(db: Session, city: City):
+def create_city(db: Session, city: City) -> City:
     city = DBCity(
         name=city.name,
         additional=city.additional,
@@ -21,11 +22,11 @@ def create_city(db: Session, city: City):
     return city
 
 
-def retrieve_city(db: Session, city_id: int):
+def retrieve_city(db: Session, city_id: int) -> City:
     return db.query(DBCity).filter(DBCity.id == city_id).first()
 
 
-def update_city(db: Session, city_id: int, city: City):
+def update_city(db: Session, city_id: int, city: City) -> City:
     city_to_update = db.query(DBCity).filter(DBCity.id == city_id).first()
     if city_to_update:
         city_to_update.name = city.name
