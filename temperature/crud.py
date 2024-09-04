77b0fd7e8e316567cf2af603db8e4a7c8e96ba64
timur <stukantimur811@gmail.com>
2024-09-04 10:18:32 +0300

    added annotations

--- temperature/crud.py
+++ temperature/crud.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from sqlalchemy import select, insert
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import Session
+from typing_extensions import List
 
 from city.models import DBCity
 from temperature.models import DBTemperature
@@ -10,12 +11,10 @@ from temperature.schemas import TemperatureSchema
 from temperature.utils import get_city_temperature, api_key, get_city_temperature
 
 
-async def set_temperature(db: AsyncSession):
+async def set_temperature(db: AsyncSession) -> None:
     async with db.begin():
         result = await db.execute(select(DBCity))
         all_city = result.scalars().all()
-
-        print(f"Found cities: {len(all_city)}")
 
         for city in all_city:
             temperature = await get_city_temperature(city_name=city.name)
@@ -28,12 +27,10 @@ async def set_temperature(db: AsyncSession):
             await db.execute(new_temperature)
         await db.commit()
 
-        print(f"Added temperature: {new_temperature}")
 
-
-def get_all_temperature(db: Session):
+def get_all_temperature(db: Session) -> List[DBTemperature]:
     return db.query(DBTemperature).all()
 
 
-def get_temperature_for_city(db: Session, city_id: int):
+def get_temperature_for_city(db: Session, city_id: int) -> DBTemperature:
     return db.query(DBTemperature).filter(DBTemperature.city_id == city_id).all()
