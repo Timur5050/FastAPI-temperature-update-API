2993b2475722826f11a97023792ce96602e67204
timur <stukantimur811@gmail.com>
2024-09-04 10:14:51 +0300

    updated crud for creating temperature statistics

--- temperature/crud.py
+++ temperature/crud.py
@@ -1,31 +1,34 @@
 from datetime import datetime
 
-from sqlalchemy import select
+from sqlalchemy import select, insert
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import Session
 
 from city.models import DBCity
 from temperature.models import DBTemperature
 from temperature.schemas import TemperatureSchema
-from temperature.utils import get_location_key, get_city_temperature, api_key
+from temperature.utils import get_city_temperature, api_key, get_city_temperature
 
 
 async def set_temperature(db: AsyncSession):
-    result = await db.execute(select(DBCity))
-    all_city = result.scalars().all()
+    async with db.begin():
+        result = await db.execute(select(DBCity))
+        all_city = result.scalars().all()
 
-    for city in all_city:
-        location_key = await get_location_key(city_name=city.name, api_key=api_key)
-        temperature = await get_city_temperature(location_key=location_key, api_key=api_key)
-        if temperature is not None:
-            new_temperature = DBTemperature(
+        print(f"Found cities: {len(all_city)}")
+
+        for city in all_city:
+            temperature = await get_city_temperature(city_name=city.name)
+
+            new_temperature = insert(DBTemperature).values(
                 city_id=city.id,
                 date_time=datetime.now(),
                 temperature=temperature,
             )
-            db.add(new_temperature)
-            await db.commit()
-            await db.refresh(new_temperature)
+            await db.execute(new_temperature)
+        await db.commit()
+
+        print(f"Added temperature: {new_temperature}")
 
 
 def get_all_temperature(db: Session):
